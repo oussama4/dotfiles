416771f034870f401f65f6789184f00aa2131b80
osama zowaki <oussamazouaki4@gmail.com>
2022-03-14 20:11:27 +0100

    add new neovim keymaps config

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,26 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+-- leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+
+-- change window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- resize
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files()<cr>", opts)
+keymap("n", "<c-t>", "<cmd>Telescope live_grep()<cr>", opts)
+
+keymap("n", "<leader>d", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
